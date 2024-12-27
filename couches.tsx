70cd9af72f6e8fa46549648059f107ba852d191e
tenboshi <tenboshi@gmail.com>
2024-12-27 13:55:34 +0900

    delete office2

---
+++ couches.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="couches" tilewidth="32" tileheight="32" tilecount="6716" columns="92">
+ <image source="../wa-village-master/tilesets/couches.png" width="2946" height="2336"/>
+</tileset>
---
+++ decor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="decor" tilewidth="32" tileheight="32" tilecount="1248" columns="39">
+ <image source="../wa-village-master/tilesets/decor.png" width="1248" height="1030"/>
+</tileset>
